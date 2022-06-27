9655fe37f9c40c72dc282394cfc865e187903103
Kevin Alejandro Frias Sanchez <alejandro.atem@gmail.com>
2022-06-26 19:54:04 -0500

    Added the functionality to see and search for pokemon, also the option to change pages

---
+++ src/components/Footer.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const Footer = () => {
+    const getYear = () => {
+        let currentYear = new Date().getFullYear();
+        return currentYear;
+    }
+   
+}
+
+export default Footer
---
+++ src/components/Navbar.jsx
@@ -0,0 +1,12 @@
+import React, { useContext } from 'react'
+import FavoriteContext from '../context/favoriteContext'
+
+const Navbar = () => {
+
+    const {favoritePokemons} = useContext(FavoriteContext)
+    const imgHead = "https://fontmeme.com/permalink/220108/1a247c1fb56f1a666cbd5c1dd8f6b82c.png"
+
+    
+}
+
+export default Navbar
